5105870bb607cbd51a2fedb2cd61768511f55a05
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-04-17 20:13:51 +0530

    timeout

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.end('Hello world form NodeJS server');
 });
-
+server.timeout=20000;
 server.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
